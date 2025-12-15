25911a8e2d152581c37075ee2878e1bbd9fa8fed
codingarmy45 <135152967+codingarmy45@users.noreply.github.com>
2025-12-15 17:08:07 +0000

    reset

--- src/components/01_Counter/Counter.jsx
+++ src/components/01_Counter/Counter.jsx
@@ -7,6 +7,7 @@ const Counter = () => {
       <button onClick={()=>setCounter(counter+1)}>+</button>
       <h1>{counter}</h1>
       <button onClick={()=>setCounter(counter-1)}>-</button>
+      <button onClick={()=>setCounter(0)}>Reset</button>
     </div>
   )
 }
